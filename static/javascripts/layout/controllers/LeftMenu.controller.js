ecd4818b47e9d41379080da492244f167d542b79
slawekK91 <kowalskislawomirkarol@gmail.com>
2015-06-07 22:25:50 +0200

    koszyk  - trzeba poprawić jeszcze layout

--- static/javascripts/layout/controllers/LeftMenu.controller.js
+++ static/javascripts/layout/controllers/LeftMenu.controller.js
@@ -1,3 +1,38 @@
 /**
  * Created by slawek on 2015-06-05.
  */
+
+(function () {
+    'use strict';
+
+    angular.module('application.layout.controllers').controller('LeftMenuController', LeftMenuController);
+
+    LeftMenuController.$inject = ['$scope', 'CategoryService', '$location']
+
+    function LeftMenuController($scope, CategoryService, $location) {
+        var vm = this;
+        vm.result = {};
+        vm.isActive = isActive;
+
+        function isActive(path) {
+            var c = path === $location.path();
+            return c;
+        }
+
+        activate()
+
+        function activate() {
+            CategoryService.getCategories().then(loadCategoriesSuccess, loadCategoriesFailure);
+        }
+
+        function loadCategoriesSuccess(data, status, headers, config) {
+
+            vm.result = data;
+        }
+
+        function loadCategoriesFailure(data, status, headers, config) {
+            console.log(data.data);
+            vm.result = data;
+        }
+    }
+})();
