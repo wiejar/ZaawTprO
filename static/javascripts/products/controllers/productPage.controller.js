0d1e6c950b51fdf388d31139ec4fa0481be8bc59
slawekK91 <kowalskislawomirkarol@gmail.com>
2015-06-07 22:25:50 +0200

    poprawa layoutu

--- static/javascripts/products/controllers/productPage.controller.js
+++ static/javascripts/products/controllers/productPage.controller.js
@@ -9,11 +9,15 @@
 
     angular.module('application.products.controllers').controller('productPage', productPage);
 
-    productPage.$inject = ['$scope', '$sce', 'ProductService', '$routeParams', 'Utileeer'];
+    productPage.$inject = ['$scope', '$sce', 'ProductService', '$routeParams', 'Utileeer', 'BasketService'];
 
-    function productPage($scope, $sce, ProductService, $routeParams, Utileeer) {
+    function productPage($scope, $sce, ProductService, $routeParams, Utileeer, BasketService) {
         var vm = this;
         vm.isNotEmpty = Utileeer.isNotEmpty;
+        vm.quantity = 1;
+        vm.basic = basic;
+        vm.examples = 'example';
+        vm.buyProduct = buyProduct;
 
         var basic = {
             name: "burger",
@@ -25,15 +29,16 @@
         };
 
         activate();
-        //ProductService.getProduct()
+
         function activate() {
             var name = $routeParams.productName;
             ProductService.getProduct(name).then(successGetProduct, failGetProduct);
         }
 
-        vm.quantity = 1;
-        vm.basic = basic;
-        vm.examples = 'example';
+        function buyProduct() {
+            console.log('elo');
+            BasketService.add(vm.basic.id, vm.quantity);
+        }
 
 
         function successGetProduct(data, status, headers, config) {
