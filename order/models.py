f45ade9b7937f15d9551f5bbad2049357f916596
Jarek <w.jaroslaw@gmail.com>
2015-06-10 19:52:44 +0200

    modul order wstepnie cz. dalsza

--- order/models.py
+++ order/models.py
@@ -7,57 +7,49 @@ from product.models import Product
 from abc import ABCMeta
 
 
-class State(models.Model, metaclass=ABCMeta):
-    name = models.CharField(max_length=20)
+class StateModel(models.Model):
+    pass#name = models.CharField(max_length=50)
 
-    def __new__(cls, *args, **kwargs):
-        raise Exception("Cannot create object State!")
+
+class State(metaclass=ABCMeta):
+    pass
 
 class StateAccepted(State):
-    def __init__(self):
-        self.name = "Accepted"
-
-    @classmethod
-    def go(cls, order):
-        order._state = StateWaitingForPayment
-
-    @classmethod
-    def back(cls, order):
-        order._state = StateError
+    pass
 
 class StateWaitingForPayment(State):
-    name = "Waiting for payment"
+    pass
 
 
-class StateError(State,models.Model):
-    name = "Error"
+class StateError(State):
+    pass
 
-#
-# class StatePaymentSuccess(State,models.Model):
-#     pass
-#
-#
-# class StateProcessing(State, models.Model):
-#     pass
-#
 
-#
-# class StateSent(State,models.Model):
-#     pass
-#
-#
-# class StateCancelled(State,models.Model):
-#     pass
-#
-#
+class StatePaymentSuccess(State):
+    pass
+
+
+class StateProcessing(State):
+    pass
+
+
+class StateSent(State):
+    pass
+
+
+class StateCancelled(State):
+    pass
+
+
 class Order(models.Model):
-    #TODO: dorobić wzorzec stan
-    status = models.ForeignKey(State)
-    # owner = models.ForeignKey(Account)
-    # shippingAddress = models.TextField()
-    # postalCode = models.TextField()
-    # city = models.TextField()
-    # totalprice = models.DecimalField(max_digits=10, decimal_places=2)
+    #TODO: zmienić na wzorzec STAN
+    state = models.CharField(max_length=50)
+    owner = models.ForeignKey(Account)
+    shippingAddress = models.TextField()
+    postalCode = models.TextField()
+    city = models.TextField()
+    totalprice = models.DecimalField(max_digits=10, decimal_places=2)
+    additional_information = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     last_change = models.DateTimeField(auto_now=True)
 
--- order/serializers.py
+++ order/serializers.py
@@ -3,9 +3,15 @@
 __author__ = 'Jarek'
 
 from rest_framework import serializers
-from order.models import Order
+from order.models import Order, State, ProductOrder
 from authentication.serializers import AccountSerializer
 from product.serializers import ProductSerializers
+
+
+class StateSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = State
+        fields = ('id', 'name',)
 
 
 class OrderSerializers(serializers.ModelSerializer):
@@ -14,13 +20,16 @@ class OrderSerializers(serializers.ModelSerializer):
     class Meta:
         model = Order
 
-        fields = ('id','owner','shippingAddress', 'postalCode', 'city' , 'totalprice','created_at','updated_at',)
+        fields = ('id', 'state','owner','shippingAddress', 'postalCode', 'city' , 'totalprice', 'additional_information','created_at','updated_at',)
         read_only_fields = ('id','created_at','updated_at')
 
 
 class ProductOrderSerializers(serializers.ModelSerializer):
-    order = OrderSerializers(many=False, read_only=True, required=True)
-    product = ProductSerializers(many=True, read_only=True, required=True)
+    order = OrderSerializers(many=False, read_only=False, required=True)
+    product = ProductSerializers(many=True, read_only=False, required=True)
 
-    fields = ('id', 'category', 'name', 'shortDesc', 'price', 'uniqueName', 'pictures',)
-    read_only_fields = ('id', 'created_at', 'updated_at')
+    class Meta:
+        model = ProductOrder
+
+        fields = ('id', 'category', 'name', 'shortDesc', 'price', 'uniqueName', 'pictures',)
+        read_only_fields = ('id', 'created_at', 'updated_at')
